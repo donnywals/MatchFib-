ae2b6df4ff1c76c3060341af24dad69a49c4d65c
Donny Wals <donny.wals@looklive.com>
2016-05-30 13:17:50 +0200

    minor test refactoring

--- MatchFibTests/MatchFibTests.swift
+++ MatchFibTests/MatchFibTests.swift
@@ -12,29 +12,26 @@ import XCTest
 class MatchFibTests: XCTestCase {
     
 /*
- Maak een 50x50 grid. Als je klikt op een lege cel, worden alle cellen
- in de rij en kolom van de cel op 1 gezet. Is de cel niet leeg, dan
- wordt +1 gedaan. Na elke verandering licht een cel kort geel op. Als 5
- elkaar in de Fibonacci-reeks opvolgende getallen naast elkaar staan,
- lichten deze cellen kort groen op en worden ze leeg gemaakt.
+Na elke verandering licht een cel kort geel op.
+
+Als 5 elkaar in de Fibonacci-reeks opvolgende getallen naast elkaar staan,
+lichten deze cellen kort groen op en worden ze leeg gemaakt.
 */
     
-    func createGrid(rows rows: Int, columns: Int) -> Grid {
-        return Grid(rows: rows, columns: columns)
+    func createAndValidateGrid(rows rows: Int, columns: Int) -> Grid {
+        let grid = Grid(rows: rows, columns: columns)
+        XCTAssert(grid.rows == rows)
+        XCTAssert(grid.columns == columns)
+        return grid
     }
     
     func testGridGeneration() {
-        let grid50 = createGrid(rows: 50, columns: 50)
-        XCTAssert(grid50.rows == 50)
-        XCTAssert(grid50.columns == 50)
-        
-        let grid20 = createGrid(rows: 20, columns: 20)
-        XCTAssert(grid20.rows == 20)
-        XCTAssert(grid20.columns == 20)
+        createAndValidateGrid(rows: 50, columns: 50)
+        createAndValidateGrid(rows: 20, columns: 20)
     }
     
-    func testCellTap() {
-        var grid = createGrid(rows: 50, columns: 50)
+    func testGridIncrement() {
+        var grid = createAndValidateGrid(rows: 50, columns: 50)
         let tappingPoint = CGPointMake(2, 2)
         grid.incrementFromPoint(tappingPoint)
         for point in grid.points {
