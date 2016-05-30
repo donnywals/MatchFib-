0f5cfaecc041ffd58cde1f492b15b82fdb19555c
Donny Wals <donny.wals@looklive.com>
2016-05-30 12:18:14 +0200

    Initial Commit

---
+++ MatchFibTests/MatchFibTests.swift
@@ -0,0 +1,36 @@
+//
+//  MatchFibTests.swift
+//  MatchFibTests
+//
+//  Created by Donny Wals on 30/05/16.
+//  Copyright © 2016 DonnyWals. All rights reserved.
+//
+
+import XCTest
+@testable import MatchFib
+
+class MatchFibTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
