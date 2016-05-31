15497e16bf3d8e60019923a79c5b885fe5c1d2d6
Donny Wals <donny.wals@looklive.com>
2016-05-31 10:41:44 +0200

    improved the unit testing

--- MatchFibTests/MatchFibTests.swift
+++ MatchFibTests/MatchFibTests.swift
@@ -44,50 +44,78 @@ lichten deze cellen kort groen op en worden ze leeg gemaakt.
     }
     
     func valuesForRow(row: Int, grid: Grid) -> [Int] {
-        return grid.pointsInRow(0).map { $0.value }
+        return grid.pointsInRow(row).map { $0.value }
     }
     
     func testFibonacciRowDetection() {
-        var grid = createAndValidateGrid(rows: 2, columns: 5)
+        var grid = createAndValidateGrid(rows: 5, columns: 5)
 
         grid.incrementFromPoint(CGPointMake(1, 1))
         XCTAssert(valuesForRow(0, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [1, 1, 1, 1, 1])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 1, 0, 0, 0])
         
         grid.incrementFromPoint(CGPointMake(2, 1))
         XCTAssert(valuesForRow(0, grid: grid) == [0, 1, 1, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [2, 2, 2, 2, 2])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 1, 1, 0, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 1, 1, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 1, 1, 0, 0])
         
         grid.incrementFromPoint(CGPointMake(3, 1))
         grid.incrementFromPoint(CGPointMake(3, 1))
         XCTAssert(valuesForRow(0, grid: grid) == [0, 1, 1, 2, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [4, 4, 4, 4, 4])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 1, 1, 2, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 1, 1, 2, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 1, 1, 2, 0])
         
         grid.incrementFromPoint(CGPointMake(4, 1))
         grid.incrementFromPoint(CGPointMake(4, 1))
         grid.incrementFromPoint(CGPointMake(4, 1))
         // fibonacci was detected, back to all 0
         XCTAssert(valuesForRow(0, grid: grid) == [0, 0, 0, 0, 0])
-    }
-    
-    func valuesForColumn(column: Int, grid: Grid) -> [Int] {
-        return grid.pointsInColumn(column).map { $0.value }
+        XCTAssert(valuesForRow(1, grid: grid) == [7, 7, 7, 7, 7])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 0, 0, 0, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 0, 0, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 0, 0, 0, 0])
     }
     
     func testFibonacciColumnDetection() {
         var grid = createAndValidateGrid(rows: 5, columns: 2)
         
         grid.incrementFromPoint(CGPointMake(1, 1))
-        XCTAssert(valuesForColumn(0, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(0, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [1, 1, 1, 1, 1])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 1, 0, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 1, 0, 0, 0])
         
         grid.incrementFromPoint(CGPointMake(1, 2))
-        XCTAssert(valuesForColumn(0, grid: grid) == [0, 1, 1, 0, 0])
+        XCTAssert(valuesForRow(0, grid: grid) == [0, 2, 0, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [1, 2, 1, 1, 1])
+        XCTAssert(valuesForRow(2, grid: grid) == [1, 2, 1, 1, 1])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 2, 0, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 2, 0, 0, 0])
         
         grid.incrementFromPoint(CGPointMake(1, 3))
         grid.incrementFromPoint(CGPointMake(1, 3))
-        XCTAssert(valuesForColumn(0, grid: grid) == [0, 1, 1, 2, 0])
+        XCTAssert(valuesForRow(0, grid: grid) == [0, 4, 0, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [1, 4, 1, 1, 1])
+        XCTAssert(valuesForRow(2, grid: grid) == [1, 4, 1, 1, 1])
+        XCTAssert(valuesForRow(3, grid: grid) == [2, 4, 2, 2, 2])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 4, 0, 0, 0])
         
         grid.incrementFromPoint(CGPointMake(1, 4))
         grid.incrementFromPoint(CGPointMake(1, 4))
         grid.incrementFromPoint(CGPointMake(1, 4))
         // fibonacci was detected, back to all 0
-        XCTAssert(valuesForColumn(0, grid: grid) == [0, 0, 0, 0, 0])
+        XCTAssert(valuesForRow(0, grid: grid) == [0, 7, 0, 0, 0])
+        XCTAssert(valuesForRow(1, grid: grid) == [0, 7, 0, 0, 0])
+        XCTAssert(valuesForRow(2, grid: grid) == [0, 7, 0, 0, 0])
+        XCTAssert(valuesForRow(3, grid: grid) == [0, 7, 0, 0, 0])
+        XCTAssert(valuesForRow(4, grid: grid) == [0, 7, 0, 0, 0])
     }
 }
